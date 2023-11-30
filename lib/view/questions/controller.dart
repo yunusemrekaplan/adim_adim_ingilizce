4981d43d7afe7cfa627f4000de46e14611c66bb3
yunusemrekaplan <yunusemrekaplan1@hotmail.com>
2023-12-01 01:18:03 +0300

    added button color

--- lib/view/questions/controller.dart
+++ lib/view/questions/controller.dart
@@ -1,4 +1,5 @@
 import 'package:adim_adim_turkce/model/library.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ControllerQuestions extends GetxController {
@@ -8,26 +9,48 @@ class ControllerQuestions extends GetxController {
   final _questionIndex = 0.obs;
   final _correctAnswers = 0.obs;
   final _incorrectAnswers = 0.obs;
+  final _score = 0.obs;
   final _isAnswered = false.obs;
   final _isCorrect = false.obs;
   final _isIncorrect = false.obs;
   final _isFinished = false.obs;
+  final _buttonColors = List.generate(4, (_) => Colors.blueGrey.obs);
 
   Question get question => _questions[_questionIndex.value];
   int get questionIndex => _questionIndex.value;
   int get correctAnswers => _correctAnswers.value;
   int get incorrectAnswers => _incorrectAnswers.value;
+  int get score => _score.value;
   bool get isAnswered => _isAnswered.value;
   bool get isCorrect => _isCorrect.value;
   bool get isIncorrect => _isIncorrect.value;
   bool get isFinished => _isFinished.value;
+  Color getButtonColor(int index) => _buttonColors[index].value;
 
   Future<void> fetchQuestions() async {
     _questions = category.questions;
+    _questions.shuffle();
+  }
+
+  void onButtonPressed(int index) {
+    if (question.answers[index].isCorrect) {
+      correctAnswer();
+      _buttonColors[index].value = Colors.green;
+    } else {
+      incorrectAnswer();
+      _buttonColors[index].value = Colors.red;
+    }
+    for (int i = 0; i < 4; i++) {
+      if (i != index && question.answers[i].isCorrect) {
+        _buttonColors[i].value = Colors.green;
+      }
+    }
   }
 
   void nextQuestion() {
     _questionIndex.value++;
+    _isFinished.value = _questionIndex.value == _questions.length;
+    resetButtonColors();
     _isAnswered.value = false;
     _isCorrect.value = false;
     _isIncorrect.value = false;
@@ -35,12 +58,14 @@ class ControllerQuestions extends GetxController {
 
   void correctAnswer() {
     _correctAnswers.value++;
+    _score.value += question.points;
     _isCorrect.value = true;
     _isAnswered.value = true;
   }
 
   void incorrectAnswer() {
     _incorrectAnswers.value++;
+    _score.value -= question.points;
     _isIncorrect.value = true;
     _isAnswered.value = true;
   }
@@ -53,9 +78,18 @@ class ControllerQuestions extends GetxController {
     _questionIndex.value = 0;
     _correctAnswers.value = 0;
     _incorrectAnswers.value = 0;
+    _score.value = 0;
     _isAnswered.value = false;
     _isCorrect.value = false;
     _isIncorrect.value = false;
     _isFinished.value = false;
+    _questions.shuffle();
+    resetButtonColors();
+  }
+
+  void resetButtonColors() {
+    for (var color in _buttonColors) {
+      color.value = Colors.blueGrey;
+    }
   }
 }
--- lib/view/questions/screen.dart
+++ lib/view/questions/screen.dart
@@ -1,7 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'controller.dart';
+
+import '../../model/library.dart' as model;
+import '../../view/widget/library.dart' as widgets;
 
 class Screen extends StatelessWidget {
   Screen({super.key});
@@ -13,7 +17,8 @@ class Screen extends StatelessWidget {
     return GetBuilder(
       init: _controller,
       builder: (_) => buildScaffold(),
-      initState: (_) => _controller.reset(),
+      initState: (_) => _controller.fetchQuestions(),
+      id: 'questions',
     );
   }
 
@@ -23,8 +28,165 @@ class Screen extends StatelessWidget {
         title: const Text('Questions'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Categories'),
+      body: Column(
+        children: [
+          Obx(
+            () => Text(
+              'Score: ${_controller.score}',
+              style: const TextStyle(fontSize: 24),
+            ),
+          ),
+          Expanded(
+            child: Obx(
+              () => _controller.isFinished ? buildFinished() : buildQuestion(),
+            ),
+          ),
+          Obx(
+            () =>
+                _controller.isFinished ? buildResetButton() : buildNextButton(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildQuestion() {
+    return Column(
+      children: [
+        buildQuestionText(),
+        buildAnswers(),
+      ],
+    );
+  }
+
+  Widget buildQuestionText() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        width: double.infinity,
+        height: Get.height * 0.4,
+        child: Center(
+          child: Text(
+            _controller.question.text,
+            //style: const TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildAnswers() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0, bottom: 10.0),
+              child: widgets.AnswerButton(
+                answer: _controller.question.answers[0],
+                buttonColor: _controller.getButtonColor(0),
+                onPressed: () => _controller.onButtonPressed(0),
+                isAnswered: _controller.isAnswered,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 25.0, bottom: 10.0),
+              child: widgets.AnswerButton(
+                answer: _controller.question.answers[1],
+                buttonColor: _controller.getButtonColor(1),
+                onPressed: () => _controller.onButtonPressed(1),
+                isAnswered: _controller.isAnswered,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+                padding: const EdgeInsets.only(left: 25.0, bottom: 10.0),
+                child: widgets.AnswerButton(
+                  answer: _controller.question.answers[2],
+                  buttonColor: _controller.getButtonColor(2),
+                  onPressed: () => _controller.onButtonPressed(2),
+                  isAnswered: _controller.isAnswered,
+                )),
+            Padding(
+              padding: const EdgeInsets.only(right: 25.0, bottom: 10.0),
+              child: widgets.AnswerButton(
+                answer: _controller.question.answers[3],
+                buttonColor: _controller.getButtonColor(3),
+                onPressed: () => _controller.onButtonPressed(3),
+                isAnswered: _controller.isAnswered,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget buildAnswerButton({
+    required model.Answer answer,
+    required VoidCallback onPressed,
+    required Color buttonColor,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Obx(
+        () => ElevatedButton(
+          onPressed: _controller.isAnswered
+              ? () {
+                  print('missclick');
+                }
+              : onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: buttonColor,
+          ),
+          child: Text(answer.text),
+        ),
+      ),
+    );
+  }
+
+  Widget buildNextButton() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: _controller.isAnswered ? _controller.nextQuestion : null,
+        child: const Text(
+          'Next',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget buildFinished() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Finished!',
+            style: TextStyle(fontSize: 24),
+          ),
+          Text(
+            'Score: ${_controller.score}',
+            style: const TextStyle(fontSize: 24),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildResetButton() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: _controller.reset,
+        child: const Text('Reset'),
       ),
     );
   }
