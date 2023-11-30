110fffb50805ca3d7a7df5c52547df936b73aa90
yunusemrekaplan <yunusemrekaplan1@hotmail.com>
2023-12-01 00:34:02 +0300

    added answer_button

---
+++ lib/view/widget/answer_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../../model/library.dart' as model;
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.answer,
+    required this.onPressed,
+    required this.buttonColor,
+    required this.isAnswered,
+  });
+
+  final model.Answer answer;
+  final VoidCallback onPressed;
+  final Color buttonColor;
+  final bool isAnswered;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: isAnswered
+            ? () {
+                print('missclick');
+              }
+            : onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: buttonColor,
+        ),
+        child: Text(answer.text),
+      ),
+    );
+  }
+}
--- lib/view/widget/library.dart
+++ lib/view/widget/library.dart
@@ -1,3 +1,4 @@
 library widgets;
 
+export 'answer_button.dart';
 export 'my_bottom_navigation_bar.dart';
