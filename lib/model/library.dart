a03b117d4dd8d1f00ae823b7306b61bf77cb857c
yunusemrekaplan <yunusemrekaplan1@hotmail.com>
2023-12-06 01:11:26 +0300

    .

--- lib/model/library.dart
+++ lib/model/library.dart
@@ -1,10 +1,11 @@
 library model;
 
 export 'answer.dart';
-export 'category.dart';
+export 'category/category.dart';
 export 'profile.dart';
 export 'student.dart';
-export 'word_question_category.dart';
+export 'category/word_question_category.dart';
 export 'question/question.dart';
 export 'question/word_question.dart';
 export 'question/fill_in_the_blank_question.dart';
+export 'question/sound_question.dart';
