785444446b942cd91ee9143d6132c74be23a8e5f
yunusemrekaplan <yunusemrekaplan1@hotmail.com>
2023-12-14 03:53:24 +0300

    Add FirestoreServiceType enum and extension

--- lib/controller/firebase/firestore/firestore_service.dart
+++ lib/controller/firebase/firestore/firestore_service.dart
@@ -1,6 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+enum FirestoreServiceType {
+  user,
+  category,
+  question,
+}
+
+extension FirestoreServiceTypeExtension on FirestoreServiceType {
+  String get name {
+    switch (this) {
+      case FirestoreServiceType.user:
+        return 'users';
+      case FirestoreServiceType.category:
+        return 'categories';
+      case FirestoreServiceType.question:
+        return 'questions';
+    }
+  }
+}
+
 class FirestoreService {
   static final FirestoreService _instance = FirestoreService._internal();
 
@@ -12,7 +31,6 @@ class FirestoreService {
 
   FirebaseFirestore? _db;
   final FirebaseApp _app = Firebase.app();
-  final _users = FirebaseFirestore.instance.collection('users');
 
   Future<void> initialize() async {
     try {
@@ -23,30 +41,35 @@ class FirestoreService {
     }
   }
 
-  Future<DocumentSnapshot<Map<String, dynamic>>?> getDocument({
+  Future<Map<String, dynamic>?> get({
+    required FirestoreServiceType type,
     required String uid,
   }) async {
     if (_db == null) await initialize();
-    DocumentSnapshot<Map<String, dynamic>>? document;
-    final ref = _users.doc(uid);
+    DocumentSnapshot<Map<String, dynamic>> document;
+    Map<String, dynamic>? data;
+    final ref = _db!.collection(type.name).doc(uid);
     try {
       document = await ref.get();
+      data = document.data();
     } on Exception catch (e) {
       print('FirestoreService.getDocument: $e');
     }
-    return document;
+    return data;
   }
 
-  Future<List<Map<String, dynamic>?>> getDocuments() async {
+  Future<List<Map<String, dynamic>?>> getList({
+    required FirestoreServiceType type,
+  }) async {
     if (_db == null) await initialize();
+
     List<Map<String, dynamic>?> documents = [];
+
     try {
-      QuerySnapshot<Map<String, dynamic>> querySnapshot = await _users.get();
+      //QuerySnapshot<Map<String, dynamic>> querySnapshot = await _users.get();
 
+      final querySnapshot = await _db!.collection(type.name).get();
       documents = querySnapshot.docs.map((e) => e.data()).toList();
-      //documents = await _users.get().then((value) => value.docs);
-
-      //documents = await _users.doc();
     } on Exception catch (e) {
       print('FirestoreService.getDocuments: $e');
     }
